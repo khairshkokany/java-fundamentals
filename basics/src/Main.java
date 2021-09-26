37572c6eedaecf2ca248f8919d1efbe4d4b79537
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-09-26 18:22:29 +0300

    flipheads

--- basics/src/Main.java
+++ basics/src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         int turtleCount = 0;
         System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+        flipNHeads(2);
     }
 
      public static String pluralize(String Str, int inter) {
@@ -17,5 +19,26 @@ public class Main {
              return Str;
          }
      }
+
+     public static void flipNHeads(int inters) {
+
+int flip =0 ;
+int heads = 0;
+while ( heads!=inters){
+
+             if (Math.random() < 0.5) {
+                 heads=0;
+                 flip++;
+                 System.out.println("tails");
+             }else {
+                 heads++;
+                 flip++;
+                 System.out.println("heads");
+
+             }
+         }
+         System.out.println( ("It took " + flip + "flips to flip" + inters + "head in a row "));
+
+}
 
 }
