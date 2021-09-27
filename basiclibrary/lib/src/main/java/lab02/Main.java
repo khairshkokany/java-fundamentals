b51ff19c52320c7bba6abdade31b3bc963e25c27
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-09-27 16:46:58 +0300

    rolls functions

---
+++ basiclibrary/lib/src/main/java/lab02/Main.java
@@ -0,0 +1,17 @@
+package lab02;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.println("hello world");
+        Library library = new Library();
+        System.out.println(Arrays.toString(library.roll(4)));
+
+
+    }
+
+
+}
